da8a1c3a2e8f8f458b6b76e010ec622973375923
AgPriyanshu18 <agpriyanshu18@gmail.com>
2023-03-20 00:18:18 +0530

    User UI and NavController Done

--- app/src/main/java/com/example/sevayu/MainActivity.kt
+++ app/src/main/java/com/example/sevayu/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.sevayu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.sevayu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var binding : ActivityMainBinding ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.bottomNavigationView!!.setupWithNavController(
+            findViewById<View>(R.id.NavHostFragment).findNavController()
+        )
 
     }
 }
